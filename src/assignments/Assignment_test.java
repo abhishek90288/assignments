790ee6423004d2be3f677ef5cf852a2b131002af
abhishek90288 <abhishek.kulkarni1987@gmail.com>
2025-08-08 19:11:20 +0530

    Assignment-1

---
+++ src/assignments/Assignment_test.java
@@ -0,0 +1,12 @@
+package assignments;
+
+public class Assignment_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World !");
+
+	}
+
+}
